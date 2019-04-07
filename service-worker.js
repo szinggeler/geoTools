e238f86a17130b3ab2df0a46e631d0ba43c0daf5
Stephan Zinggeler <stephan@zinggeler.net>
2019-04-07 23:32:54 +0200

    Adjust service-worker and map layers

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,66 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+// workbox.setConfig({modulePathPrefix: './build/workbox-v4.2.0'});
+/*
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+*/
+
+workbox.precaching.precacheAndRoute([
+    './geoToolsParcel.3baf5dcc.js',
+    './geoToolsParcel.5cd39c0a.css',
+    './manifest.json',
+    './icoGeoTools192.png',
+    './icoGeoTools512.png',
+    './geotools.html'
+]);
+
+// ??? ev. besser als jetzt mit Locale Storage ???
+/*
+workbox.routing.registerRoute(
+  new RegExp('.*geoservices\.zh\.ch\/geoshopapi\/v1\/products.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'fpc-cache',
+  })
+);
+*/
+
+workbox.routing.registerRoute(
+  new RegExp('.*fonts\.googleapis\.com.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'font-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*fonts\.gstatic\.com.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'font-cache',
+  })
+);
+
+/*
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  new workbox.strategies.NetworkFirst()
+);
+*/
+
+/*
+workbox.routing.registerRoute(
+  // Cache CSS files.
+  /\.css$/,
+  // Use cache but update in the background.
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+*/
+
+/*
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('progessive-elm').then(function(cache) {
@@ -32,3 +95,4 @@ self.addEventListener('fetch', function(event) {
 
     );
 });
+*/
